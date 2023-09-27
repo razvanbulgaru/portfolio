71b02ffa83f27c953ef1e4ae6896fe054578cbbe
Razvan Bulgaru <razvan.bulgaru@gmail.com>
2023-09-27 15:13:25 +0300

    Intro component

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion';
 import { links } from '@/lib/data';
 import Link from 'next/link';
 
-export const Header = () => {
+const Header = () => {
 	return (
 		<header className="z-[999] relative">
 			<motion.div
@@ -12,26 +12,26 @@ export const Header = () => {
 				initial={{ y: -100, x: '-50%', opacity: 0 }}
 				animate={{ y: 0, x: '-50%', opacity: 1 }}
 			>
-				<motion.nav className="flex items-center justify-center h-12 sm:h-[initial] py-2 sm:py-0">
-					<motion.ul className="flex flex-wrap justify-center items-center w-[22rem] gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
-						{links.map((link) => {
-							return (
-								<motion.li
-									className="h-3/4 flex items-center justify-center"
-									key={link.hash}
+				<nav className="flex items-center justify-center h-12 sm:h-[initial] py-2 sm:py-0">
+					<ul className="flex flex-wrap justify-center items-center w-[22rem] gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
+						{links.map((link) => (
+							<li
+								className="h-3/4 flex items-center justify-center"
+								key={link.hash}
+							>
+								<Link
+									className="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition"
+									href={link.hash}
 								>
-									<Link
-										className="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition"
-										href={link.hash}
-									>
-										{link.name}
-									</Link>
-								</motion.li>
-							);
-						})}
-					</motion.ul>
-				</motion.nav>
+									{link.name}
+								</Link>
+							</li>
+						))}
+					</ul>
+				</nav>
 			</motion.div>
 		</header>
 	);
 };
+
+export default Header;

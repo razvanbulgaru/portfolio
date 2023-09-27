dbefc8561b85cf0f361a4cdd4e60674795d5f36b
Razvan Bulgaru <razvan.bulgaru@gmail.com>
2023-09-27 16:58:51 +0300

    Projects component

---
+++ components/Projects.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import SectionHeading from './SectionHeading';
+import { projectsData } from '@/lib/data';
+import Project from './Project';
+
+const Projects = () => {
+	return (
+		<section>
+			<SectionHeading>My Projects</SectionHeading>
+			<div>
+				{projectsData.map((project, index) => (
+					<React.Fragment key={index}>
+						<Project {...project} />
+					</React.Fragment>
+				))}
+			</div>
+		</section>
+	);
+};
+
+export default Projects;

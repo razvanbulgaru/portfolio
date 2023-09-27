3d0dea14fa5dfb044c5969ea7ec08ef3d322c235
Razvan Bulgaru <razvan.bulgaru@gmail.com>
2023-09-27 15:19:47 +0300

    Section devider component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Intro from '@/components/Intro';
+import SectionDevider from '@/components/SectionDevider';
 
 export default function Home() {
 	return (
 		<main className="flex flex-col items-center px-4">
 			<Intro />
+			<SectionDevider />
 		</main>
 	);
 }

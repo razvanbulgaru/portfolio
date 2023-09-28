ba97d1945bcddff600436e40ae6166e4b9589553
Razvan Bulgaru <razvan.bulgaru@gmail.com>
2023-09-28 16:53:31 +0300

    Apply duplicate style

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -79,7 +79,7 @@ const Intro = () => {
 					<BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
 				</Link>
 				<a
-					className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10"
+					className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack"
 					href="/CV.pdf"
 					download
 				>
@@ -87,14 +87,14 @@ const Intro = () => {
 					<HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
 				</a>
 				<a
-					className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full outline-none focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border-black/10"
+					className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full outline-none focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack"
 					href="https://www.linkedin.com/in/razvan-bulgaru-0aa033213/"
 					target="_blank"
 				>
 					<BsLinkedin />
 				</a>
 				<a
-					className="bg-white p-4 text-gray-700 text-[1.35rem] flex items-center gap-2 rounded-full outline-none focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border-black/10"
+					className="bg-white p-4 text-gray-700 text-[1.35rem] flex items-center gap-2 rounded-full outline-none focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack"
 					href="https://github.com/razvanbulgaru"
 					target="_blank"
 				>
